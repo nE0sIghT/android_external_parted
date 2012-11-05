2a2ee733978bd3120658fd3fd140529bae360fbe
Andrew Boie <andrew.p.boie@intel.com>
2012-11-05 10:40:30 -0800

    parted: Add gnulib
    This is present in the parted release tarball but not the git
    repository and is required to run parted. Its use is approved in
    the IP plan request for parted.
    
    Android.mk added to build with Android.
    
    Source:
    http://ftp.gnu.org/gnu/parted/parted-3.1.tar.xz
    
    Signature:
    -----BEGIN PGP SIGNATURE-----
    Version: GnuPG v1.4.12 (GNU/Linux)
    
    iQIcBAABCAAGBQJPUQeUAAoJEH/Z/MsAC+7uwS0P/iplXKYgLCZ7PXhAy2QWSEgC
    yEDNUN2hdK5RqXPzmyzTEObflRRkVQV8nWLBg9r5jCXKBKl0OEiBDSd9jwbdUSwI
    6ueSsbrLLpwnQkqBsh7F9zwIJ2xDsPYmhOqxTbXpITn8fv9MAMPFm0+pGYgIPC4o
    JITcP+8ufjjQpdecv/EmiM+3rrcMxgVi7/F158uj+ih4mxl7L4APG2hIib+Xdd8x
    lqzVogAQoW6n5uoQo/F2zcGv/NPMH7LqwH435VEzc3FTEKQ4ArZ6o/hYasMALW2F
    iXyjxQKDPBmiXOkKwmhRCBQtFVk1rpy6JSw07ljL2sZlScnKvq3AA9ufomdlgtIR
    vBDRjkJeB1W12sk75eQRiP1RS3+TT1rpo9kvE4a1iZkMWrHPvaCDKM48MdN/Nvb6
    nLafTM2FsHPEZ9HJCmEY4LRH0zmTJQy8KW5XSHFJgEhk22Jk5S951uTkM1sEbvHL
    RRZHIGs/RZwetIUvFFEE87hZJ6OWO0XVStUnA+ik255XgN8Ik5o4K50hI3qU0/e3
    3faMq6MpBYfQn5z1SO99/TE70hjyi8Q5E+SpuF3qn4cfhYi3zn8e6OaGNLVOXcWU
    5/9WI5v9a5br/74RmIElsieetnMvlpbe9feAvsziQooNPe584qlQq7SXVegcXGNu
    MUrTB6SjxheBtLqH0PlX
    =0NA7
    -----END PGP SIGNATURE-----
    
    Change-Id: Ia4570a65fbfcdb4a9ef44388b756996bc736fe18
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

---
+++ lib/Android.mk
@@ -0,0 +1,30 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	xmalloc.c \
+	xalloc-die.c \
+	argmatch.c \
+	progname.c \
+	closeout.c \
+	c-strcasecmp.c \
+	error.c \
+	exitfail.c \
+	close-stream.c \
+	quote.c \
+	quotearg.c \
+	fpending.c \
+	version-etc.c \
+	version-etc-fsf.c \
+	canonicalize-lgpl.c \
+	malloca.c \
+	basename-lgpl.c \
+	xstrtoll.c \
+
+LOCAL_CFLAGS :=
+LOCAL_MODULE := libgnulib
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include
+
+include $(BUILD_STATIC_LIBRARY)
+
